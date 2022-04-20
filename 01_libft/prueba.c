e9babb5820c19489294179c1628890a4a55d5836
Zachary Alexander iZ Williams <onextthenitromancer@gmail.com>
2022-04-21 00:10:25 +0200

    Sugerencias pa mañana

--- 01_libft/prueba.c
+++ 01_libft/prueba.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+//Probar a no incluir el stdio llamando a printf Y sin llamar a printf a ver si algo que use size_t fufa sin él.
+//#include <stdio.h>
 #include "libft.h"
 
 int main(void)
