6c629d4030768cad2ff81e99e13b9b14dbcf2cc6
Nestor Tamayo franco <ntamayo-@c2r2s3.42malaga.com>
2022-04-21 20:21:38 +0200

    Strlcat and changes to libft.h

--- libft/strlcpy_strlcat.c
+++ libft/strlcpy_strlcat.c
@@ -6,12 +6,12 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 19:51:59 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/04/20 20:41:14 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/04/21 20:12:05 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <string.h>
+//#include <string.h>
 #include "libft.h"
 
 size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
@@ -28,12 +28,30 @@ size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 	return (ft_strlen(src));
 }
 
-//size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize);
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	size_t	dstlen;
+	int		nulcond;
 
+	nulcond = 0;
+	if (dstsize && (dstlen <= dstsize))
+		nulcond = 1;
+	dstlen = ft_strlen(dst);
+	dst += dstlen;
+	while (dstsize - dstlen - 1 && src)
+	{
+		*dst++ = *src++;
+		dstsize--;
+	}
+	if (nulcond)
+		*dst = 0;
+	return (ft_strlen(dst) + ft_strlen(src));
+}
+/*
 int	main(void)
 {
 	char a[] = "12345";
 	char b[] = "abcde";
 	ft_strlcpy(b, a, 3);
 	printf("%s\n", b);
-}
+}*/
