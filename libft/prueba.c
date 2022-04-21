fa7c9c74d49afa404d58fad8ec809e8409a1b927
Nestor Tamayo franco <ntamayo-@c2r2s3.42malaga.com>
2022-04-21 20:29:57 +0200

    Misc changes

--- libft/prueba.c
+++
@@ -1,8 +0,0 @@
-//Probar a no incluir el stdio llamando a printf Y sin llamar a printf a ver si algo que use size_t fufa sin él.
-//#include <stdio.h>
-#include "libft.h"
-
-int main(void)
-{
-	printf("%d", ft_isalpha('a'));
-}
