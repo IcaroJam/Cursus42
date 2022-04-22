920006dbfb316d084e4e7dfa203c731fe7604e84
Nestor Tamayo franco <ntamayo-@c2r7s2.42malaga.com>
2022-04-22 21:33:49 +0200

    Changes to strlcpy and strcat

--- libft/strlcpy_strlcat.c
+++ libft/strlcpy_strlcat.c
@@ -6,51 +6,63 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 19:51:59 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/04/22 10:33:59 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/04/22 17:16:10 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
-//#include <string.h>
+//#include <stdio.h>
 #include "libft.h"
 
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
+	size_t	srclen;
+
+	srclen = ft_strlen(src);
 	if (dstsize)
 	{
-		while (dstsize - 1 && src)
+		while (dstsize - 1 && *src)
 		{
 			*dst++ = *src++;
 			dstsize--;
 		}
 		*dst = 0;
 	}
-	return (ft_strlen(src));
+	return (srclen);
 }
 
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dstlen;
+	size_t	srclen;
 	int		nulcond;
 
 	nulcond = 0;
-	if (dstsize && (dstlen <= dstsize))
-		nulcond = 1;
 	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	if (dstlen >= dstsize)
+		return (dstsize + srclen);
+	if (dstsize)
+		nulcond = 1;
 	dst += dstlen;
-	while (dstsize - dstlen - 1 && src)
+	while ((dstsize - dstlen - 1) && *src)
 	{
 		*dst++ = *src++;
 		dstsize--;
 	}
 	if (nulcond)
 		*dst = 0;
-	return (ft_strlen(dst) + ft_strlen(src));
+	return (dstlen + srclen);
 }
-/*
+
+/*#include <string.h>
 int	main(void)
 {
-	char a[] = "12345";
-	char b[] = "abcde";
-	ft_strlcpy(b, a, 3);
-	printf("%s\n", b);
+	char *str = "the cake is a lie !\0I'm hidden lol\r\n";
+//	char buff1[0xF00] = "there is no stars in the sky";
+	char buff2[0xF00] = "there is no stars in the sky";
+	size_t max = ft_strlen("the cake is a lie !\0I'm hidden lol\r\n") + 4;
+//	char a[] = "12345\0.a.a.a.a.";
+//	char b[] = "abcde.-.-.-";
+	ft_strlcat(buff2, str, max);
+	printf("%s\n", buff2);
 }*/
