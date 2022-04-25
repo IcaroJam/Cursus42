64000c3a0b3cb8f3ea787a444dbe1f2fb839b61e
Nestor Tamayo franco <ntamayo-@c2r1s4.42malaga.com>
2022-04-25 20:11:18 +0200

    Implemented memcmp

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 19:06:35 by ntamayo-          #+#    #+#             */
+/*   Updated: 2022/04/25 19:58:29 by ntamayo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stddef.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*st1 = (const unsigned char *)s1;
+	const unsigned char	*st2 = (const unsigned char *)s2;
+
+	while (n--)
+	{
+		if (*st1 != *st2)
+			return (*st1 - *st2);
+		st1++;
+		st2++;
+	}
+	return (0);
+}
