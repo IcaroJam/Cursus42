5f8c4067acd53a9ec0b6e7755476db010034ded4
Nestor Tamayo franco <ntamayo-@c2r1s4.42malaga.com>
2022-04-25 15:51:38 +0200

    Split memset, bzero, memcpy and memmove

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,34 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memset_bzero_memcpy_memmove.c                      :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/19 13:48:48 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/04/23 20:16:06 by ntamayo-         ###   ########.fr       */
+/*   Created: 2022/04/25 15:49:54 by ntamayo-          #+#    #+#             */
+/*   Updated: 2022/04/25 15:50:20 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stddef.h>
-
-void	*ft_memset(void *b, int c, size_t len)
-{
-	unsigned char	*newb;
-
-	newb = b;
-	while (len--)
-		*newb++ = c;
-	return (b);
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	char	*temps;
-
-	temps = s;
-	while (n--)
-		*temps++ = 0;
-}
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
@@ -42,22 +24,4 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	while (n--)
 		*d++ = *s++;
 	return (dst);
-}
-
-void	*ft_memmove(void *dst, const void *src, size_t len)
-{
-	unsigned char		*d;
-	const unsigned char	*s;
-
-	d = dst;
-	s = src;
-	if (!d && !s)
-		return (0);
-	if (d < s)
-		while (len--)
-			*d++ = *s++;
-	else
-		while (len--)
-			d[len] = s[len];
-	return ((unsigned char *)dst);
 }
