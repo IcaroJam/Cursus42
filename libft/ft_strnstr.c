d642bd42326aa868b50be1d69488eabab4918dfa
Nestor Tamayo franco <ntamayo-@c2r1s4.42malaga.com>
2022-04-25 21:35:45 +0200

    Removed unnecessary include

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,11 +6,10 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 21:00:44 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/04/25 21:33:37 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/04/25 21:35:16 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
-#include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
