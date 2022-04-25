048bba0b57e2b41cf87b125207b51e41ff2e42eb
Nestor Tamayo franco <ntamayo-@c2r1s4.42malaga.com>
2022-04-25 15:46:14 +0200

    Split isascii, isprint and update Makefile

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -1,21 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ascii_print.c                                      :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/19 13:31:31 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/04/19 19:24:39 by ntamayo-         ###   ########.fr       */
+/*   Created: 2022/04/25 15:45:21 by ntamayo-          #+#    #+#             */
+/*   Updated: 2022/04/25 15:45:42 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_isascii(int c)
-{
-	if (c >= 0 && c <= 127)
-		return (1);
-	return (0);
-}
 
 int	ft_isprint(int c)
 {
@@ -23,11 +16,3 @@ int	ft_isprint(int c)
 		return (1);
 	return (0);
 }
-/*
-#include <stdio.h>
-#include <ctype.h>
-
-int main(void)
-{
-	printf("%d %d %d %d", ft_isascii(1), isascii(1), ft_isprint(1), isprint(1));
-}*/
