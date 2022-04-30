5f3e00b5458094280523409cb95181e4290287ab
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-04-30 19:14:31 +0200

    Implemented bonus stuff and lstnew

---
+++ libft/ft_lstnew_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/30 17:08:30 by ntamayo-          #+#    #+#             */
+/*   Updated: 2022/04/30 19:10:32 by ntamayo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*ret;
+
+	ret = (t_list *) malloc(sizeof(t_list));
+	if (!ret)
+		return (NULL);
+	(*ret).content = content;
+	(*ret).next = NULL;
+	return (ret);
+}
