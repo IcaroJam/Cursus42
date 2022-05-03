84ab89b29c3bef7db781c8872b909be380fdabed
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-03 11:00:40 +0200

    Implemented lstadd_back & unbonused the bonuses

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 13:31:57 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/05/03 09:58:52 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/05/03 10:08:20 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,6 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
