15197f1bfbf8aee2ee9e639688a8ceb815ef6660
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-03 16:48:40 +0200

    Implemented lstiter

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 13:31:57 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/05/03 11:59:26 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/05/03 12:23:48 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del) (void *));
 void	ft_lstclear(t_list **lst, void (*del) (void *));
+void	ft_lstiter(t_list *lst, void (*f) (void *));
 
 #endif
