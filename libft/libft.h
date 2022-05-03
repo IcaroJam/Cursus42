8cf2d75a8587168d87992e55e4dbb89d1c7ec7fd
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-03 12:22:11 +0200

    Implemented lstclear

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 13:31:57 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/05/03 11:06:45 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/05/03 11:59:26 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,6 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del) (void *));
+void	ft_lstclear(t_list **lst, void (*del) (void *));
 
 #endif
