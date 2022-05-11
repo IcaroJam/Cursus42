3236f5c38a97009ae3d19cf03c574fa61545bb32
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-11 18:49:36 +0200

    GNL2 starting (near to none) progress

--- gnl2/get_next_line.c
+++ gnl2/get_next_line.c
@@ -6,13 +6,21 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 13:49:42 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/05/11 18:12:05 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/05/11 18:49:20 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	buffprocess()
+
 char	*get_next_line(int fd)
 {
+	static char	*buff = NULL;
+	char		*ret;
 
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0))
+		return (NULL);
+
+	return (ret);
 }
