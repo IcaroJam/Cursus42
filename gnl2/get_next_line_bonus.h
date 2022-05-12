a815bfd5b425c0dc050476f935fae2c3273dac86
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-12 13:02:50 +0200

    Almost perfect bonus
    Output errors on fd change inbetween lines at BUFFER_SIZE 100000

---
+++ gnl2/get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/04 13:47:59 by ntamayo-          #+#    #+#             */
+/*   Updated: 2022/05/12 12:56:13 by ntamayo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+void	ft_strlcpy(char *dst, char *src, int size);
+int		ft_linelen(char *mem);
+int		ft_strlen(char *s);
+int		lecture_master(int fd, char **time_machine);
+char	*the_line_maker(char **time_machine);
+char	*get_next_line(int fd);
+
+#endif
