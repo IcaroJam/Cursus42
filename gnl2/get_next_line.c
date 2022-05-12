f5d4b1c220cd06d84e99249cc9670667f0cd3c6b
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-12 12:50:41 +0200

    T O T A L   P R O G R E S S   (no bonus yet tho)

--- gnl2/get_next_line.c
+++ gnl2/get_next_line.c
@@ -6,21 +6,80 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 13:49:42 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/05/11 18:49:20 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/05/12 12:49:23 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	buffprocess()
+int	lecture_master(int fd, char **time_machine)
+{
+	int		eoflag;
+	char	buffer[BUFFER_SIZE + 1];
+	char	*temp;
+
+	if (!*time_machine)
+	{
+		*time_machine = malloc(sizeof(char));
+		if (!*time_machine)
+			return (-1);
+		**time_machine = 0;
+	}
+	eoflag = 1;
+	while (!ft_strchr(*time_machine, '\n') && eoflag)
+	{
+		eoflag = read(fd, buffer, BUFFER_SIZE);
+		if (eoflag < 1)
+			return (eoflag);
+		buffer[eoflag] = 0;
+		temp = *time_machine;
+		*time_machine = ft_strjoin(*time_machine, buffer);
+	}
+	return (eoflag);
+}
+
+char	*the_line_maker(char **time_machine)
+{
+	int		len;
+	int		temporal_distortion;
+	char	*tmach2;
+	char	*retline;
+
+	len = ft_linelen(*time_machine);
+	if (!len)
+		return (NULL);
+	retline = malloc(sizeof(char) * (len + 1));
+	if (!retline)
+		return (NULL);
+	ft_strlcpy(retline, *time_machine, len);
+	temporal_distortion = ft_strlen(&time_machine[0][len]);
+	tmach2 = malloc(sizeof(char) * (temporal_distortion + 1));
+	if (!tmach2)
+		return (NULL);
+	ft_strlcpy(tmach2, &time_machine[0][len], temporal_distortion);
+	free(*time_machine);
+	*time_machine = tmach2;
+	return (retline);
+}
 
 char	*get_next_line(int fd)
 {
-	static char	*buff = NULL;
+	static char	*time_machine = NULL;
 	char		*ret;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0))
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-
+	if (lecture_master(fd, &time_machine) < 0)
+	{
+		free(time_machine);
+		time_machine = NULL;
+		return (NULL);
+	}
+	ret = the_line_maker(&time_machine);
+	if (!ret)
+	{
+		free(time_machine);
+		time_machine = NULL;
+	}
 	return (ret);
 }
--- gnl2/get_next_line_utils.c
+++ gnl2/get_next_line_utils.c
@@ -6,9 +6,78 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 12:33:23 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/05/11 18:12:28 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/05/12 12:38:49 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	ft_strlen(char *s)
+{
+	int	ret;
+
+	ret = 0;
+	while (s[ret])
+		ret++;
+	return (ret);
+}
+
+int	ft_linelen(char *mem)
+{
+	int	ret;
+
+	ret = 0;
+	while (mem[ret] && mem[ret] != '\n')
+		ret++;
+	if (mem[ret] == '\n')
+		ret++;
+	return (ret);
+}
+
+void	ft_strlcpy(char *dst, char *src, int size)
+{
+	if (!dst || !src)
+		return ;
+	while (size)
+	{
+		*dst++ = *src++;
+		size--;
+	}
+	*dst = 0;
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	unsigned char	temp;
+
+	temp = c;
+	while (*s || !temp)
+	{
+		if (*s == temp)
+			return ((char *)s);
+		s++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		s1len;
+	int		s2len;
+	char	*ret;
+
+	if (!s1 || !s2)
+		return (0);
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	ret = (char *) malloc((s1len + s2len + 1) * sizeof(char));
+	if (!ret)
+		return (0);
+	while (*s1)
+		*ret++ = *s1++;
+	while (*s2)
+		*ret++ = *s2++;
+	*ret = 0;
+	free(s1 - s1len);
+	return (ret - s1len - s2len);
+}
