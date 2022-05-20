514c1ab1bef5cda273e265e10f4d512cfedf3fd6
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-20 15:31:31 +0200

    Create the foundations

---
+++ printf/libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/20 15:20:59 by ntamayo-          #+#    #+#             */
+/*   Updated: 2022/05/20 15:23:36 by ntamayo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <stdlib.h>
+# include <stdarg.h>
+
+int	ft_printf(char const *, ...);
+
+#endif
