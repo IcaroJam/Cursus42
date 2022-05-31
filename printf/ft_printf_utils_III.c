a9c88aa5494a864181ed53e6c12e5efe0b347741
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-31 18:39:21 +0200

    Bonus-proof char B)

--- printf/ft_printf_utils_III.c
+++ printf/ft_printf_utils_III.c
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 15:11:58 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/05/31 16:55:19 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/05/31 18:38:57 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,41 @@ char	*ptf_chars(t_flags flags, va_list list)
 {
 	char	*ret;
 	int		c;
+	int		i;
 
-	if (flags.dot)
-		flags.minfw = 0;
-	ret = malloc(sizeof(char) * (2 + flags.minfw));
+	if (!flags.minfw)
+		flags.minfw = 1;
+	ret = malloc(sizeof(char) * (1 + flags.minfw));
 	if (!ret)
 		return (NULL);
 	c = va_arg(list, int);
-	ret[0] = c;
-	ret[1] = 0;
+//	ret[0] = c;
+//	ret[1] = 0;
+	i = 0;
+	if (flags.dash)
+	{
+		ret[i++] = c;
+		while (i < flags.minfw)
+			ret[i++] = ' ';
+	}
+	else
+	{
+		flags.minfw--;
+		while (i < flags.minfw)
+			ret[i++] = ' ';
+		ret[i++] = c;
+	}
+	ret[i] = 0;
 	return (ret);
 }
 
-char	*ptf_string(t_flags flags, va_list list)
-{
-	char	*ret;
-	char	*str;
-
-	str = va_arg(list, char *);
-}
+/** char	*ptf_string(t_flags flags, va_list list)
+  * {
+  *     char	*ret;
+  *     char	*str;
+  *
+  *     str = va_arg(list, char *);
+  * } */
 
 void	ptf_putstr(t_pbuff *buffer)
 {
