f3bd237d29c8f59c2b4aaa759508cb61ec738528
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-06-16 11:49:21 +0200

    Fix norm error

--- so_long/render_utils.c
+++ so_long/render_utils.c
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 11:45:52 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/06/16 11:47:25 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/06/16 11:49:01 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ void	ft_mlx_pixel_put(t_image *img, int x, int y, int color)
 	char	*dst;
 
 	dst = img->addr + (y * img->line_size + x * (img->bpp / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *) dst = color;
 }
