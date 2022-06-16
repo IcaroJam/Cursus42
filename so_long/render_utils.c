32b3d6a1d17e723c0837a348595968271fd344b4
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-06-16 11:47:48 +0200

    Create rendering pixel-by-pixel function

---
+++ so_long/render_utils.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 11:45:52 by ntamayo-          #+#    #+#             */
+/*   Updated: 2022/06/16 11:47:25 by ntamayo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	ft_mlx_pixel_put(t_image *img, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = img->addr + (y * img->line_size + x * (img->bpp / 8));
+	*(unsigned int*)dst = color;
+}
