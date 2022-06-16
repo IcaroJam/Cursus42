28826711e9b731d62caa3eb477739a42b9e496a9
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-06-16 12:10:38 +0200

    Rename ft_mlx_pixel_put to ft_pxlp

--- so_long/render_utils.c
+++ so_long/render_utils.c
@@ -6,13 +6,13 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 11:45:52 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/06/16 11:49:01 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/06/16 12:10:05 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	ft_mlx_pixel_put(t_image *img, int x, int y, int color)
+void	ft_pxlp(t_image *img, int x, int y, int color)
 {
 	char	*dst;
 
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 11:26:18 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/06/16 11:52:43 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/06/16 12:09:48 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,6 @@ typedef struct s_image
 	int		endian;
 }	t_image;
 
-void	ft_mlx_pixel_put(t_image *img, int x, int y, int color);
+void	ft_pxlp(t_image *img, int x, int y, int color);
 
 #endif
