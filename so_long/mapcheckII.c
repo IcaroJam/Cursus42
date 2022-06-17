e560acd36c976e0bc66c95981364f1c0ce56efb8
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-06-17 12:26:31 +0200

    Fixed norm errors.

--- so_long/mapcheckII.c
+++ so_long/mapcheckII.c
@@ -6,17 +6,14 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 11:58:14 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/06/17 11:59:59 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/06/17 12:25:52 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	mapprocess(t_map *map)
+void static	top(t_map *map)
 {
-	int	irows;
-	int	iclms;
-
 	while (*map->str != '\n')
 	{
 		if (*map->str != '1')
@@ -24,33 +21,54 @@ void	mapprocess(t_map *map)
 		map->str++;
 		map->clms++;
 	}
-	map->str++;
-	irows = 2;
-	while (irows < map->rows)
-	{
-		iclms = 0;
-		if (*map->str != '1')
-			printerror("Map error: LEFT WALL BREACH!!!");
-		while (*map->str != '\n')
-		{
-			map->str++;
-			iclms++;
-		}
-		if (map->str[-1] != '1')
-			printerror("Map error: RIGHT WALL BREACH!!!");
-		if (iclms != map->clms)
-			printerror("Map error: Wall shift???");
-		map->str++;
-		irows++;
-	}
-	iclms = 0;
+}
+
+void static	bottom(t_map *map, int	*iclms)
+{
 	while (*map->str != '\n')
 	{
 		if (*map->str != '1')
 			printerror("Map error: LOWER WALL BREACH!!!");
 		map->str++;
-		iclms++;
+		(*iclms)++;
 	}
+}
+
+void static	midrows(t_map *map, int	*irows, int *iclms)
+{
+	while (*irows < map->rows)
+	{
+		*iclms = 0;
+		if (*map->str != '1')
+			printerror("Map error: LEFT WALL BREACH!!!");
+		while (*map->str != '\n')
+		{
+			map->str++;
+			(*iclms)++;
+		}
+		if (map->str[-1] != '1')
+			printerror("Map error: RIGHT WALL BREACH!!!");
+		if (*iclms != map->clms)
+			printerror("Map error: Wall shift???");
+		map->str++;
+		(*irows)++;
+	}
+}
+
+void	mapprocess(t_map *map)
+{
+	int		irows;
+	int		iclms;
+	char	*originalstr;
+
+	originalstr = map->str;
+	top(map);
+	map->str++;
+	irows = 2;
+	midrows(map, &irows, &iclms);
+	iclms = 0;
+	bottom(map, &iclms);
 	if (iclms != map->clms)
 		printerror("Map error: Wall shift???");
+	map->str = originalstr;
 }
