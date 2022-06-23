22277c0f7e932e06515ebbd0812d8ba557ab1dad
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-06-23 15:34:07 +0200

    Enemy animation.

--- so_long/animation.c
+++ so_long/animation.c
@@ -6,11 +6,36 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 12:48:35 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/06/23 11:23:45 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/06/23 15:33:13 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void static	playeranim(t_program *mlx)
+{
+	if (mlx->player.cursprt == mlx->playsprt.frame0)
+		mlx->player.cursprt = mlx->playsprt.frame1;
+	else
+		mlx->player.cursprt = mlx->playsprt.frame0;
+	mlx->map.tile[mlx->player.ypos][mlx->player.xpos].update = 1;
+}
+
+void static	foeanim(t_program *mlx)
+{
+	int	i;
+
+	i = 0;
+	while (i < mlx->map.foes)
+	{
+		if (mlx->foelst[i].cursprt == mlx->foesprt.frame0)
+			mlx->foelst[i].cursprt = mlx->foesprt.frame1;
+		else
+			mlx->foelst[i].cursprt = mlx->foesprt.frame0;
+		mlx->map.tile[mlx->foelst[i].ypos][mlx->foelst[i].xpos].update = 1;
+		i++;
+	}
+}
 
 int	animate(void *program)
 {
@@ -21,11 +46,8 @@ int	animate(void *program)
 		return (1);
 	if (mlx->frame == 3599)
 	{
-		if (mlx->player.cursprt == mlx->playsprt.frame0)
-			mlx->player.cursprt = mlx->playsprt.frame1;
-		else
-			mlx->player.cursprt = mlx->playsprt.frame0;
-		mlx->map.tile[mlx->player.ypos][mlx->player.xpos].update = 1;
+		playeranim(mlx);
+		foeanim(mlx);
 		rendermap(mlx);
 	}
 	mlx->frame++;
