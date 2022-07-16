f06fe576040cb36b77b9cf9ff083aff65c058611
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-07-16 12:21:50 +0200

    Changed order of the total lenght multiplication to size*count.

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 12:53:14 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/07/14 18:30:12 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/07/16 12:20:52 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*start;
 
-	start = malloc(count * size);
+	start = malloc(size * count);
 	if (!start)
 		return (NULL);
-	ft_bzero(start, count * size);
+	ft_bzero(start, size * count);
 	return (start);
 }
