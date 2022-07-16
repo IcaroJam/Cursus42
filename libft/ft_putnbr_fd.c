bb257bd0cd522af44971a4bb5d9b4ffef2c1df92
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-07-16 14:38:00 +0200

    Prettyfied putnbr_fd.

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,16 +6,11 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 11:40:33 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/04/30 11:57:24 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/07/16 14:36:33 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-static void	ft_putchr_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
+#include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
 {
@@ -27,10 +22,10 @@ void	ft_putnbr_fd(int n, int fd)
 	else if (n < 0)
 	{
 		n = -n;
-		write(fd, "-", 1);
+		ft_putchar_fd('-', fd);
 	}
 	if (n / 10 == 0)
-		ft_putchr_fd(n + '0', fd);
+		ft_putchar_fd(n + '0', fd);
 	else
 	{
 		ft_putnbr_fd(n / 10, fd);
