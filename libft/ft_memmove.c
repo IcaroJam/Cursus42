3b28c438836f92ea7364967ad8dc2e5c834b79f5
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-07-16 13:32:28 +0200

    Prettyfied memmove.

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 15:50:30 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/05/03 09:48:44 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/07/16 13:30:41 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char		*d;
-	const unsigned char	*s;
+	const unsigned char	*s = src;
 
 	d = dst;
-	s = src;
 	if (!d && !s)
-		return (0);
+		return (NULL);
 	if (d < s)
 		while (len--)
 			*d++ = *s++;
 	else
 		while (len--)
 			d[len] = s[len];
-	return ((unsigned char *)dst);
+	return (dst);
 }
