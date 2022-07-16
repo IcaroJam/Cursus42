05ad7958f1069d2ac509212f1577481b35cf93ae
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-07-16 12:51:30 +0200

    Update and fix security issues.

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 10:01:41 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/05/03 11:42:36 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/07/16 12:50:53 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*temp;
 
-	if (!lst || !*lst)
+	if (!lst)
+		return ;
+	if (!*lst)
 	{
 		*lst = new;
 		return ;
 	}
 	temp = ft_lstlast(*lst);
-	(*temp).next = new;
+	temp->next = new;
+	temp->next->next = NULL;
 }
