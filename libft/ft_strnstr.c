faae455025bd769ab05be75c774415a22e0a47c6
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-07-18 13:10:04 +0200

    Small improvements.

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 21:00:44 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/05/11 13:19:04 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/07/18 13:02:19 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,22 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	templen;
 
 	if (!*needle)
-		return ((char *)haystack);
+		return ((char *) haystack);
 	while (*haystack && len)
 	{
 		j = 0;
 		templen = len;
-		while (templen && needle[j] && haystack[j] && haystack[j] == needle[j])
+		while (templen && haystack[j] && haystack[j] == needle[j])
 		{
 			if (!needle[j + 1])
-				return ((char *)haystack);
+				return ((char *) haystack);
 			j++;
 			templen--;
 			if (templen == 0)
-				return (0);
+				return (NULL);
 		}
 		len--;
 		haystack++;
 	}
-	return (0);
+	return (NULL);
 }
