5d7ef10be9bb73b7a56e1c82a326a9b894e280a3
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-07-19 15:35:39 +0200

    Check input duplicates implemented.

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,11 +6,30 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 12:19:56 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/07/19 14:41:17 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/07/19 15:00:39 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void static	checkdups(char **argv)
+{
+	int	head;
+	int	search;
+
+	head = 1;
+	while (argv[head])
+	{
+		search = 0;
+		while (search < head)
+		{
+			if (!ft_strncmp(argv[head], argv[search], ft_strlen(argv[head])))
+				errmsg("Duplicates present.");
+			search++;
+		}
+		head++;
+	}
+}
 
 int static	inputhandler(int argc, char **argv)
 {
@@ -19,6 +38,7 @@ int static	inputhandler(int argc, char **argv)
 	if (argc == 1)
 		return (1);
 	argv++;
+	checkdups(argv);
 	while (*argv)
 	{
 		i = 0;
@@ -38,8 +58,7 @@ int	main(int argc, char **argv)
 {
 	if (!inputhandler(argc, argv))
 	{
-		write(1, "a", 1);
-	// DO STUFF
+		return (1);
 	}
 	return (0);
 }
