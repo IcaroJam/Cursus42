7ac6d85d3e10ccf192219b9b90ac2f7f0d089e0f
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-07-20 18:29:32 +0200

    First sa, sb and ss implementation.

--- push_swap/instructions/swap.c
+++ push_swap/instructions/swap.c
@@ -6,8 +6,25 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 18:11:56 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/07/20 18:12:11 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/07/20 18:21:27 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+void	ps_swap(t_stack *stack)
+{
+	int	temp;
+
+	if (stack->top <= 1)
+		return ;
+	temp = stack->stk[stack->top - 1];
+	stack->stk[stack->top - 1] = stack->stk[stack->top - 2];
+	stack->stk[stack->top - 2] = temp;
+}
+
+void	ps_ss(t_stack *a, t_stack *b)
+{
+	ps_swap(a);
+	ps_swap(b);
+}
