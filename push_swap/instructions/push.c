e53989e2e5202115f703b62f506b72459d2af0fa
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-07-20 18:29:50 +0200

    First pa and pb implementation.

--- push_swap/instructions/push.c
+++ push_swap/instructions/push.c
@@ -6,8 +6,15 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 18:12:25 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/07/20 18:12:30 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/07/20 18:28:01 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+void	ps_push(t_stack *p_from, t_stack *p_to)
+{
+	if (!p_from->top)
+		return ;
+	p_to->stk[p_to->top++] = p_from->stk[--p_from->top];
+}
