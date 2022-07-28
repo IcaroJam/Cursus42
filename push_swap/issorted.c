610ee57e5f7058deee47500396a306e455559154
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-07-28 13:06:44 +0200

    Added function to return index of smallest number.

--- push_swap/issorted.c
+++ push_swap/issorted.c
@@ -6,11 +6,32 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 12:19:38 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/07/28 12:30:03 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/07/28 12:48:12 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	getsmol(t_stack *stack)
+{
+	int	temp;
+	int	i;
+	int	ret;
+
+	i = 0;
+	ret = i;
+	temp = stack->stk[i++].num;
+	while (i < stack->top)
+	{
+		if (stack->stk[i].num < temp)
+		{
+			ret = i;
+			temp = stack->stk[i].num;
+		}
+		i++;
+	}
+	return (ret);
+}
 
 int	ispartsorted(t_stack stack)
 {
