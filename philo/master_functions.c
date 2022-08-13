c930ac0691479c8c15d00072b33016724abaddc5
senari <onextthenitromancer@gmail.com>
2022-08-13 14:34:28 +0200

    Got rid of some redundant ifs.

--- philo/master_functions.c
+++ philo/master_functions.c
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 11:27:43 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/08/13 14:29:29 by senari           ###   ########.fr       */
+/*   Updated: 2022/08/13 14:33:07 by senari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,7 @@ void static	munchtime(t_prg *prg, t_philosopher *cphl)
 		cphl->timeseaten++;
 		cphl->lstmealtime = timesince(prg);
 	}
-	if (!prg->philodeath)
-		phisleep(prg, prg->tte);
+	phisleep(prg, prg->tte);
 	pthread_mutex_unlock(&prg->forks[cphl->leftfork]);
 	pthread_mutex_unlock(&prg->forks[cphl->rightfork]);
 }
@@ -59,8 +58,7 @@ void	*sofic_routine(void *thelot)
 			munchtime(prg, cphl);
 		if (!prg->philodeath)
 			consolelog(prg, "%dms %d is sleeping\n", timesince(prg), cphl->id);
-		if (!prg->philodeath)
-			phisleep(prg, prg->tts);
+		phisleep(prg, prg->tts);
 		if (!prg->philodeath)
 			consolelog(prg, "%dms %d is thinking\n", timesince(prg), cphl->id);
 	}
