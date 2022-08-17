f9fa77c1582aa457cf7dc54753864b99e919d090
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-08-17 15:10:52 +0200

    Done.

--- philo/bonusdir/processes.c
+++ philo/bonusdir/processes.c
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 11:56:58 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/08/17 11:50:55 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/08/17 15:07:41 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ static void	solog(t_prg *prg, const char *msg, int time, int id)
 {
 	sem_wait(prg->log);
 	printf(msg, time, id);
-	sem_post(prg->log);
+	if (!prg->phls[id - 1].isdead)
+		sem_post(prg->log);
 }
 
 static void	munchtime(t_prg *prg, t_philosopher *cphl)
@@ -64,9 +65,8 @@ void	*overseer(void *everything)
 	{
 		if (timesince(prg) - cphl->lstmealtime > prg->ttd)
 		{
-			solog(prg, "%dms %d died\n", timesince(prg), cphl->id);
-			sem_wait(prg->log);
 			cphl->isdead = 1;
+			solog(prg, "%dms %d died\n", timesince(prg), cphl->id);
 			exit(1);
 		}
 		if (prg->notepme > -1 && cphl->timeseaten == prg->notepme)
