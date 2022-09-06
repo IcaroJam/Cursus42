c8e0675cae8db925dfaf175f76d8e6ccbbb242d8
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-09-06 13:08:11 +0200

    Word counting in lexer.

--- minishell/parser/lexer.c
+++ minishell/parser/lexer.c
@@ -6,17 +6,68 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 11:57:10 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/09/06 11:57:30 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/09/06 13:07:31 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../minishell.h"
+
+static int	count_isspace(const char *line)
+{
+	int	ret;
+
+	ret = 0;
+	while (ft_isspace(line[ret]))
+		ret++;
+	return (ret);
+}
+
+static int	handle_quotes(const char *line, const char flag)
+{
+	int	ret;
+
+	ret = 0;
+	while (line[ret] != flag && line[ret])
+		ret++;
+	if (!line[ret])
+		return (1);
+	return (ret);
+}
+
+static int	process_word(const char *line)
+{
+	int		ret;
+
+	ret = 0;
+	while (line[ret] && !ft_isspace(line[ret]))
+	{
+		if (line[ret] == '\'' || line[ret] == '"')
+			ret += handle_quotes(line, *line);
+		else
+			ret++;
+	}
+	return (ret);
+}
+
+static int	count_words(const char *line)
+{
+	int	ret;
+
+	ret = 0;
+	while (*line)
+	{
+		line += count_isspace(line);
+		line += process_word(line);
+		ret++;
+	}
+	return (ret);
+}
 
 char	**tokenize_line(char *line)
 {
 	char	**ret;
 
-	
+	if (!*line)
+		return (NULL);
 	return (ret);
 }
-
--- minishell/parser/parsermain.c
+++ minishell/parser/parsermain.c
@@ -6,13 +6,15 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 11:47:36 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/09/06 11:57:02 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/09/06 13:07:51 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../minishell.h"
 
-// The line inputed into the shell is divided into "tokens" (words) that are then grouped into a command table, of which the first two lines are always the input and output filedrescriptors respectively.
+// The line inputed into the shell is divided into "tokens" (words) that are
+// then grouped into a command table, of which the first two lines are always
+// the input and output filedrescriptors respectively.
 char	***parse_line(char *line)
 {
 	char	***ret;
