d8ec1aa8d2ebca570d798e28627985ae98ca9f60
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-09-07 13:37:57 +0200

    Token lenght seemingly being properly returned.

--- minishell/parser/lexer.c
+++ minishell/parser/lexer.c
@@ -6,87 +6,83 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 11:57:10 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/09/06 17:25:47 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/09/07 13:37:05 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 #include "msparser.h"
 
-static int	get_quotelen(const char **line)
+static int	get_quotelen(t_lexutil *lxu)
 {
 	int	qlen;
-	int	openquote;
 
-	qlen = 0;
-	openquote = handle_quotes(*line, **line);
-	if (openquote == 1)
-	{
-		while (**line && !ft_isspace(**line) && **line != '<' && **line != '>')
-		{
-			line[0]++;
-			qlen++;
-		}
-		return (qlen);
-	}
-	*line += openquote;
-	return (openquote - 2);
+	qlen = handle_quotes(&lxu->line[lxu->lnoff], lxu->line[lxu->lnoff]);
+	// Update posible single and double quote flags to know when to include
+	// or ignore them when storing the tokens.
+	if (!qlen)
+		return (1);
+	lxu->lnoff += qlen;
+	return (qlen - 1);
 }
 
-static int	get_tknlen(const char *line)
+static int	get_tknlen(t_lexutil *lxu)
 {
 	int	tlen;
 
 	tlen = 0;
-	if (line[0] == '<')
+	if (lxu->line[lxu->lnoff] == '<' || lxu->line[lxu->lnoff] == '>'
+		|| lxu->line[lxu->lnoff] == '|')
 	{
-		if (line [1] == '<')
-			return (8);
-		return (4);
+		lxu->lnoff = handle_redir(lxu->line);
+		return (lxu->lnoff);
 	}
-	if (line[0] == '>')
+	while (lxu->line[lxu->lnoff] && !ft_isspace(lxu->line[lxu->lnoff])
+			&& lxu->line[lxu->lnoff] != '<' && lxu->line[lxu->lnoff] != '>'
+			&& lxu->line[lxu->lnoff] != '|')
 	{
-		if (line[1] == '>')
-			return (10);
-		return (5);
-	}
-	while (*line && !ft_isspace(*line) && *line != '<' && *line != '>')
-	{
-		if (*line == '\'' || *line == '"')
-			tlen += get_quotelen(&line);
-		else if (*line != '<' && *line != '>')
+		if (lxu->line[lxu->lnoff] == '\'' || lxu->line[lxu->lnoff] == '\"')
+			tlen += get_quotelen(lxu);
+		else
 			tlen++;
+		lxu->lnoff++;
 	}
-	return (0);
+	return (tlen);
 }
 
-static char	**tokenstr(const char *line, int numow)
+static char	**tokenstr(t_lexutil *lxu, int numow)
 {
 	char	**ret;
 	int		tknlen;
 	int		i;
 
 	i = 0;
+	ret = NULL;
 	while (i++ < numow)
 	{
-		line += count_isspace(line);
-		tknlen = get_tknlen(line);
+		lxu->lnoff = 0;
+		lxu->line += count_isspace(&lxu->line[lxu->lnoff]);
+		tknlen = get_tknlen(lxu);
+		// Allocation and storing here.
+		lxu->line += lxu->lnoff;
 	}
 	return (ret);
 }
 
 char	**tokenize_line(char *line)
 {
-	char	**ret;
-	int		numow;
+	t_lexutil	lxu;
+	char		**ret;
+	int			numow;
 
 	if (!*line)
 		return (NULL);
 	numow = count_words(line);
-	ret = ft_calloc(numow, sizeof(char *));
-	if (!ret)
-		return (NULL);
-	ret = tokenstr(line, numow);
+	/** ret = ft_calloc(numow + 1, sizeof(char *));
+	  * if (!ret)
+	  *     return (NULL); */
+	lxu.line = line;
+	ret = tokenstr(&lxu, numow);
 	// Parsing errors like <<< here !!!
 	return (ret);
 }
--- minishell/parser/msparser.h
+++ minishell/parser/msparser.h
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 13:09:55 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/09/06 17:26:48 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/09/07 12:11:18 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ typedef struct s_lexutil
 int	count_isspace(const char *line);
 int	count_words(const char *line);
 int	handle_quotes(const char *line, const char flag);
+int	handle_redir(const char *line);
 
 #endif
