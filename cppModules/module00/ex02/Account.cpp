1a49c47a68936c139ae350809c755bbe223c09bc
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2022-10-06 12:07:27 +0200

    time display testing.

--- cppModules/module00/ex02/Account.cpp
+++ cppModules/module00/ex02/Account.cpp
@@ -6,11 +6,14 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 16:33:50 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/10/05 17:33:39 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/10/06 12:07:00 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <chrono>
+#include <ctime>
+#include <iostream>
 
 Account::Account( void ) {
 
@@ -22,4 +25,11 @@ Account::Account( int initial_deposit ) {
 
 Account::~Account( void ) {
 
+}
+
+int main ( void ) {
+	std::time_t	currTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	tm			currDate = *localtime(&currTime);
+
+	std::cout << currDate.tm_year << std::endl << currDate.tm_mon << std::endl << currDate.tm_mday << std::endl << currDate.tm_hour << std::endl << currDate.tm_min << std::endl << currDate.tm_sec << std::endl;
 }
