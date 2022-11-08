66aba4ba86f83697bf1469a8f888a6f12c78a1d6
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2022-11-08 12:31:36 +0100

    Fixed beginning

---
+++ cppModules/module02/ex00/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 12:25:33 by ntamayo-          #+#    #+#             */
+/*   Updated: 2022/11/08 12:25:54 by ntamayo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+int main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
