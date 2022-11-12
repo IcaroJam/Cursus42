c84778059982d0fa5f43a2e98480b87ad61c2a06
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2022-11-12 13:02:40 +0100

    More mathematical tests.

--- cppModules/module02/ex03/main.cpp
+++ cppModules/module02/ex03/main.cpp
@@ -6,22 +6,21 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 12:25:33 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/11/12 12:55:58 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/11/12 13:02:09 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
 int main( void ) {
-	// Tests shamefully taken from aaguiler
-	Point a = Point(3, 6);
-	Point b = Point(5, 2);
-	Point c = Point(7, 5);
+	Point a = Point(-2.75, 0);
+	Point b = Point(2.5, 2);
+	Point c = Point(2, -2);
 
-	Point p1 = Point(4, 5); // inside
-	Point p2 = Point(4, 4); // edge
-	Point p3 = Point(6, 3); // outside
-	Point p4 = Point(3, 6); // vertex
+	Point p1 = Point(0, -1); // inside
+	Point p2 = Point(0, 1.04761905); // edge
+	Point p3 = Point(2.5, 0); // outside
+	Point p4 = Point(2, -2); // vertex
 
 	std::cout << "p1: " << bsp(a, b, c, p1) << std::endl;
 	std::cout << "p2: " << bsp(a, b, c, p2) << std::endl;
