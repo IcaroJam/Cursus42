e393c4236aec9119c45556b68d06f7a7a1fa0876
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2022-12-04 12:45:34 +0100

    Not very pretty but functional tests.

--- cppModules/module04/ex03/main.cpp
+++ cppModules/module04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 15:45:28 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/12/02 13:42:12 by senari           ###   ########.fr       */
+/*   Updated: 2022/12/04 12:45:07 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,12 @@ int	main(void) {
 	src->learnMateria(new Cure());
 
 	ICharacter* me = new Character("me");
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
+	AMateria* mat1;
+	AMateria* mat2;
+	mat1 = src->createMateria("ice");
+	me->equip(mat1);
+	mat2 = src->createMateria("cure");
+	me->equip(mat2);
 
 	ICharacter* bob = new Character("bob");
 	me->use(0, *bob);
@@ -42,6 +43,9 @@ int	main(void) {
 	me->unequip(1);
 	me->use(1, *bob);
 	me->unequip(0);
+
+	delete mat1;
+	delete mat2;
 
 	delete bob;
 	delete me;
