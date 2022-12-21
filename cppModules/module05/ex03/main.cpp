d4a0c109d2b2db1b8b4864109b6adb79dbf6e16f
senari <onextthenitromancer@gmail.com>
2022-12-21 14:14:46 +0100

    Execution test.

--- cppModules/module05/ex03/main.cpp
+++ cppModules/module05/ex03/main.cpp
@@ -6,10 +6,11 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 15:50:13 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/12/21 14:02:20 by senari           ###   ########.fr       */
+/*   Updated: 2022/12/21 14:14:05 by senari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
 #include "Intern.hpp"
 
 void leakCheck(void) {
@@ -19,10 +20,15 @@ void leakCheck(void) {
 int	main(void) {
 	atexit(leakCheck);
 	
-	Intern	UnimportantJake;
-	Form	*intForm;
+	Bureaucrat	ImportantBuro("Jeremiah", 3);
+	Intern		UnimportantJake;
+	Form		*intForm;
 
-	intForm = UnimportantJake.makeForm("Presidential pardon", "I, myself");
+	intForm = UnimportantJake.makeForm("PresidentialpardonfOrm", "I, myself");
 
 	std::cout << *intForm << std::endl;
+	ImportantBuro.signForm(intForm);
+	ImportantBuro.execForm(*intForm);
+
+	delete intForm;
 }
