036add2b35485fa0898deeba59f29749add77bb7
senari <onextthenitromancer@gmail.com>
2022-12-24 19:06:52 +0100

    Now detecting + and processing only digit chars.

--- libft/ft_ioverflw.c
+++ libft/ft_ioverflw.c
@@ -6,11 +6,21 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 12:37:36 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/12/24 18:56:01 by senari           ###   ########.fr       */
+/*   Updated: 2022/12/24 19:05:38 by senari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	numlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (ft_isdigit(str[i]))
+		i++;
+	return (i);
+}
 
 int	ft_ioverflw(char *str, int num)
 {
@@ -18,7 +28,9 @@ int	ft_ioverflw(char *str, int num)
 
 	while (ft_isspace(*str))
 		str++;
-	len = ft_strlen(str);
+	if (*str == '+')
+		str++;
+	len = numlen(str);
 	if (*str == '-')
 		return (!num || len > 11 || (len == 11 && str[1] > '2'));
 	return (num == -1 || len > 10 || (len == 10 && str[0] > '2'));
