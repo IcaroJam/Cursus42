bc3d4500447351b1863ab2896749943a3070262e
senari <onextthenitromancer@gmail.com>
2022-12-24 19:11:06 +0100

    Lighter atoi.

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,17 +6,18 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 21:34:53 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/07/14 18:24:19 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2022/12/24 18:50:54 by senari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 static const char	*ft_prep(const char *str, int *isneg)
 {
+	*isneg = 1;
 	while ((*str > '\b' && *str < 14) || *str == ' ')
 		str++;
 	if (*str == '-')
 	{
-		*isneg = 1;
+		*isneg = -1;
 		str++;
 	}
 	else if (*str == '+')
@@ -29,7 +30,6 @@ int	ft_atoi(const char *str)
 	int	isneg;
 	int	ret;
 
-	isneg = 0;
 	ret = 0;
 	str = ft_prep(str, &isneg);
 	while (*str >= '0' && *str <= '9')
@@ -37,11 +37,10 @@ int	ft_atoi(const char *str)
 		ret *= 10;
 		ret += *str++ - '0';
 	}
-	if (isneg)
-		ret *= -1;
-	if (isneg && ret > 0)
+	ret *= isneg;
+	if (isneg == -1 && ret > 0)
 		return (0);
-	if (!isneg && ret < 0)
+	if (isneg == 1 && ret < 0)
 		return (-1);
 	return (ret);
 }
