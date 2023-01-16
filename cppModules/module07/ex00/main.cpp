29faa6916a902d7dfa020e96ca1a4ef3196581d2
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-01-16 12:18:15 +0100

    Added mandatory tests.

--- cppModules/module07/ex00/main.cpp
+++ cppModules/module07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 11:35:06 by ntamayo-          #+#    #+#             */
-/*   Updated: 2023/01/16 11:56:12 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2023/01/16 12:17:26 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 #include <iostream>
 
 int	main(void) {
+	std::cout << "/// Mandatory ///" << std::endl;
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl << std::endl;
+
+	std::cout << "/// Extra ///" << std::endl;
 	int	i1 = 7, i2 = -12;
 	std::cout	<< "i1 = " << i1 << std::endl
 				<< "i2 = " << i2 << std::endl
