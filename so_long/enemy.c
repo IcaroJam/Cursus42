d6c20aa1ba7489c27112afe01536eb6851971e2b
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-01-24 11:22:47 +0100

    Fixed enemy eating though tiles bug.

--- so_long/enemy.c
+++ so_long/enemy.c
@@ -6,37 +6,50 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 16:02:15 by ntamayo-          #+#    #+#             */
-/*   Updated: 2022/06/23 17:39:46 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2023/01/24 10:44:56 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	foeia(t_program *mlx)
+static int	getfoenewpos(t_map *map, t_foe *foe)
 {
-	int	i;
+	int	ret;
+
+	if (map->tile[foe->ypos][foe->xpos + foe->direction].type != '0'
+		&& map->tile[foe->ypos][foe->xpos + foe->direction].type != 'P')
+	{
+		foe->direction *= -1;
+	}
+	ret = foe->xpos + foe->direction;
+	return (ret);
+}
+
+static void	foelogic(t_map *map, t_foe *foe, t_player *player)
+{
 	int	xto;
 	int	yto;
 
+	xto = getfoenewpos(map, foe);
+	yto = foe->ypos;
+	if (map->tile[yto][xto].type == 'P')
+		player->isdead = 1;
+	if (map->tile[yto][xto].type == '0' || map->tile[yto][xto].type == 'P')
+	{
+		map->tile[yto][xto].type = 'B';
+		map->tile[foe->ypos][foe->xpos].type = '0';
+		map->tile[yto][xto].update = 1;
+		map->tile[foe->ypos][foe->xpos].update = 1;
+		foe->xpos = xto;
+		foe->ypos = yto;
+	}
+}
+
+void	foeia(t_program *mlx)
+{
+	int	i;
+
 	i = 0;
 	while (i < mlx->map.foes)
-	{
-		xto = mlx->foelst[i].xpos + mlx->foelst[i].direction;
-		yto = mlx->foelst[i].ypos;
-		if (mlx->map.tile[yto][xto].type == 'P')
-			mlx->player.isdead = 1;
-		else if (mlx->map.tile[yto][xto].type != '0')
-		{
-			mlx->foelst[i].direction *= -1;
-			xto = mlx->foelst[i].xpos + mlx->foelst[i].direction;
-			yto = mlx->foelst[i].ypos;
-		}
-		mlx->map.tile[yto][xto].type = 'B';
-		mlx->map.tile[mlx->foelst[i].ypos][mlx->foelst[i].xpos].type = '0';
-		mlx->map.tile[yto][xto].update = 1;
-		mlx->map.tile[mlx->foelst[i].ypos][mlx->foelst[i].xpos].update = 1;
-		mlx->foelst[i].xpos = xto;
-		mlx->foelst[i].ypos = yto;
-		i++;
-	}
+		foelogic(&mlx->map, &mlx->foelst[i++], &mlx->player);
 }
