39c5a64798b451cfc5a17423c164ea739899f05d
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-02-06 16:06:14 +0100

    Make tests more readable.

--- cppModules/module07/ex02/main.cpp
+++ cppModules/module07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 16:55:37 by ntamayo-          #+#    #+#             */
-/*   Updated: 2023/01/17 11:35:06 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2023/02/06 16:04:44 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,15 @@ int	main(void) {
 	Array<std::string>	a4;
 
 	std::cout	<< "Four empty arrays were just created." << std::endl
-				<< "a1 size: " << a1.size() << std::endl
-				<< "a2 size: " << a2.size() << std::endl
-				<< "a3 size: " << a3.size() << std::endl
-				<< "a4 size: " << a4.size() << std::endl
+				<< "a1 (int)    size: " << a1.size() << std::endl
+				<< "a2 (char)   size: " << a2.size() << std::endl
+				<< "a3 (char *) size: " << a3.size() << std::endl
+				<< "a4 (string) size: " << a4.size() << std::endl
 				<< "Attempting to access a1[0]: ";
 	try {
 		std::cout << a1[0];
 	} catch (std::exception &exep) {
-		std::cerr << exep.what() << std::endl;
+		std::cerr << exep.what() << std::endl << std::endl;
 	}
 
 	Array<int>			b1(3);
@@ -43,10 +43,10 @@ int	main(void) {
 	Array<std::string>	b4(3);
 
 	std::cout	<< "Four arrays of size 3 were just created." << std::endl
-				<< "b1 size: " << b1.size() << std::endl
-				<< "b2 size: " << b2.size() << std::endl
-				<< "b3 size: " << b3.size() << std::endl
-				<< "b4 size: " << b4.size() << std::endl
+				<< "b1 (int)    size: " << b1.size() << std::endl
+				<< "b2 (char)   size: " << b2.size() << std::endl
+				<< "b3 (char *) size: " << b3.size() << std::endl
+				<< "b4 (string) size: " << b4.size() << std::endl
 				<< "Attempting to access b1[2]: " << b1[2] << std::endl
 				<< "Attempting to access b2[2]: " << b2[2] << std::endl
 				<< "Attempting to access b3[2]: " << "(This would result in a segfault since pointers default initialize to NULL)" << std::endl
@@ -55,6 +55,6 @@ int	main(void) {
 	try {
 		std::cout << b3[3];
 	} catch (std::exception &exep) {
-		std::cerr << exep.what() << std::endl;
+		std::cerr << exep.what() << std::endl << std::endl;
 	}
 }
