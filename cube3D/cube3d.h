f9f77005e0a9dbdfaadfbb4d8d8d39fadcee23e9
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-03-07 09:51:58 +0100

    Norm fix.

--- cube3D/cube3d.h
+++ cube3D/cube3d.h
@@ -6,22 +6,28 @@
 /*   By: senari <ntamayo-@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 15:58:53 by senari            #+#    #+#             */
-/*   Updated: 2023/03/06 16:54:03 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2023/03/07 09:50:09 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUBERT_H
-# define CUBERT_H
+#ifndef CUBE3D_H
+# define CUBE3D_H
 
 # include "libft/libft.h"
 # include "MLX42/include/MLX42/MLX42.h"
 # include <stdio.h>
 
 /**
-* @brief A function that takes a string beginning by a digit character and returns the number it represent in the range [0, 255] or -1 if the value falls out of range. The character length of the number in the given string is passed onto the integer pointed to by the offset parameter ONLY if the given number falls in the range.
+* @brief A function that takes a string beginning by a digit character and
+* returns the number it represent in the range [0, 255] or -1 if the value falls
+* out of range. The character length of the number in the given string is passed
+* onto the integer pointed to by the offset parameter ONLY if the given number
+* falls in the range.
 *
-	* @param s: The string beginning by the first digit of the number to convert to int.
-	* @param offset: Pointer to an integer to store the length of the number, or NULL.
+	* @param s: The string beginning by the first digit of the number to convert
+	* to int.
+	* @param offset: Pointer to an integer to store the length of the number,
+	* or NULL.
 	*
 	* @return 
 */
