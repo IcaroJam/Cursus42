8834c7b085e73fcb4a6f1478b940af02f29f1fef
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-03-13 20:07:58 +0100

    Normalization works fine.

--- cube3D/src/parser/makeitsquared.c
+++ cube3D/src/parser/makeitsquared.c
@@ -6,7 +6,7 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 11:16:12 by ntamayo-          #+#    #+#             */
-/*   Updated: 2023/03/10 12:47:36 by senari           ###   ########.fr       */
+/*   Updated: 2023/03/13 20:07:36 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,14 +39,6 @@ static void	stretchlines(t_cub *cub)
 	i = 0;
 	while (cub->mdata.cmap[i])
 	{
-		/** while (ft_strlen(cub->mdata.cmap[i]) < cub->mdata.xsize)
-		  * {
-		  *     temp = ft_strjoin(cub->mdata.cmap[i], " ");
-		  *     if (!temp)
-		  *         frerrxit("Map stretching failed!", cub);
-		  *     free(cub->mdata.cmap[i]);
-		  *     cub->mdata.cmap[i] = temp;
-		  * } */
 		temp = malloc(sizeof(char) * (cub->mdata.xsize + 1));
 		if (!temp)
 			frerrxit("Map stretching failed!", cub);
@@ -63,8 +55,6 @@ static void	stretchlines(t_cub *cub)
 		cub->mdata.cmap[i] = temp;
 		i++;
 	}
-	for (unsigned int k = 0; k < cub->mdata.ysize; k++)
-		printf("%s| len: %lu\n", cub->mdata.cmap[k], ft_strlen(cub->mdata.cmap[k]));
 }
 
 void	makeitsquared(t_cub *cub)
