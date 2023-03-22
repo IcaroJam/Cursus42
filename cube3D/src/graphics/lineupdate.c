f7ff0ba95349f00f5f9a57f13c4d26a7c27707d4
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2023-03-22 17:35:18 +0100

    Textures working. Kinda.

--- cube3D/src/graphics/lineupdate.c
+++ cube3D/src/graphics/lineupdate.c
@@ -6,11 +6,26 @@
 /*   By: ntamayo- <ntamayo-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 15:27:42 by ntamayo-          #+#    #+#             */
-/*   Updated: 2023/03/22 16:10:57 by ntamayo-         ###   ########.fr       */
+/*   Updated: 2023/03/22 17:32:55 by ntamayo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../cube3d.h"
+
+static unsigned int	texturer(mlx_image_t *img, int lineh, int j, float texcoord)
+{
+	unsigned int	hex;
+	const float		step = (float)lineh / img->height;
+	int				pixindex;
+
+	hex = 0;
+	pixindex = 4 * (img->width * (int)(j / step) + (int)(img->width * texcoord));
+	hex |= img->pixels[pixindex] << 24;
+	hex |= img->pixels[pixindex + 1] << 16;
+	hex |= img->pixels[pixindex + 2] << 8;
+	hex |= img->pixels[pixindex + 3];
+	return (hex);
+}
 
 static void	scanline(t_cub *cub, unsigned int i)
 {
@@ -32,13 +47,13 @@ static void	scanline(t_cub *cub, unsigned int i)
 	while (j < end)
 	{
 		if (cub->sight[i].worient == -1)
-			mlx_put_pixel(cub->lines[i], 0, j, 0x995050FF);
+			mlx_put_pixel(cub->lines[i], 0, j, texturer(cub->simg, h, j - start, cub->sight[i].wtexc));
 		else if (cub->sight[i].worient == 0)
-			mlx_put_pixel(cub->lines[i], 0, j, 0x509950FF);
+			mlx_put_pixel(cub->lines[i], 0, j, texturer(cub->eimg, h, j - start, cub->sight[i].wtexc));
 		else if (cub->sight[i].worient == 2)
-			mlx_put_pixel(cub->lines[i], 0, j, 0x505099FF);
+			mlx_put_pixel(cub->lines[i], 0, j, texturer(cub->wimg, h, j - start, cub->sight[i].wtexc));
 		else
-			mlx_put_pixel(cub->lines[i], 0, j, 0x999950FF);
+			mlx_put_pixel(cub->lines[i], 0, j, texturer(cub->nimg, h, j - start, cub->sight[i].wtexc));
 		j++;
 	}
 	while (j < WINHEIGHT)
@@ -47,7 +62,7 @@ static void	scanline(t_cub *cub, unsigned int i)
 
 void	lineupdate(t_cub *cub)
 {
-	unsigned int	i;
+    unsigned int	i;
 
 	i = 0;
 	ft_raycasting(cub);
@@ -55,5 +70,5 @@ void	lineupdate(t_cub *cub)
 	{
 		scanline(cub, i);
 		i++;
-	}
+    }
 }
