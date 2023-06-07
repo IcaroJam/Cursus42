2cd965f3609c4bb202ae8541b455c2352987f0de
ntamayo- <ntamayo-@student.42malaga.com>
2023-06-07 19:30:38 +0200

    Oh my fucking god it is done. Never again.

--- inception/srcs/requirements/mariadb/tools/dascript.sh
+++ inception/srcs/requirements/mariadb/tools/dascript.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-RESULT=$(mysql -u root -e "use $MYSQL_DB")
+mv /my.cnf /etc/mysql/my.cnf
 
-if [[ -z "$RESULT" ]]
-then
 mysql_install_db
 /etc/init.d/mysql start
 
@@ -18,10 +16,8 @@ y
 y
 EOF
 
-< /tableup.scripture mysql -u root
+echo "GRANT ALL ON *.* TO '$MYSQL_ROOT_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES; CREATE DATABASE IF NOT EXISTS $MYSQL_DB; CREATE USER '$MYSQL_USER'@'wordpress.inception_incepnet' IDENTIFIED BY '$MYSQL_PASSWORD'; GRANT ALL ON *.* TO '$MYSQL_USER'@'wordpress.inception_incepnet'; FLUSH PRIVILEGES;" | mysql -u root
+
 sleep 3
 /etc/init.d/mysql stop
 sleep 3
-fi
-
-mysqld
--- inception/srcs/requirements/wordpress/tools/pressup.sh
+++ inception/srcs/requirements/wordpress/tools/pressup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if ! [[ php wp-cli.phar --info ]]
+if ! [[ -f /var/www/wordpress/wp-config.php ]]
 then
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o /usr/local/bin/wp
-	chmod +x /usr/local/bin/wp
+	curl -o /temp.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x /temp.phar
+	mv /temp.phar /usr/local/bin/wp
+
 	wp core download --path=/var/www/wordpress --allow-root
 
 	cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/wordpress/wp-config.php
-	sed -i "s/localhost/mariadb.inception_my_network/g" /var/www/wordpress/wp-config.php
+	sed -i "s/localhost/mariadb.inception_incepnet/g" /var/www/wordpress/wp-config.php
 	sed -i "s/username_here/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
 	sed -i "s/database_name_here/$MYSQL_DB/g" /var/www/wordpress/wp-config.php
 
@@ -20,4 +22,7 @@ then
 		--role=author --allow-root --path=/var/www/wordpress
 fi
 
-php-fpm7 -F
+mkdir -p /run/php
+chown -R wpuser /var/www/wordpress && chmod -R 775 /var/www/wordpress
+
+php-fpm7.3 -y /etc/php/7.3/fpm/php-fpm.conf -F
